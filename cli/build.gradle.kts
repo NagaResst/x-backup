2a3a0dd916764625b209eff1ff031f3b10b8ad8b
Naga Resst <nagaresst@live.com>
2026-05-05 00:27:08 +0800

    fix: 将 CLI 模块 JVM 版本从 17 升级到 21 以解决依赖兼容性问题

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -16,16 +16,16 @@ buildscript {
 apply(plugin = "java")
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 tasks.withType<JavaCompile>().configureEach {
-    options.release = 17
+    options.release = 21
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 @Suppress("PackageUpdate")
